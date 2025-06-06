d6e0ff79144d13b49cf953256d128359dc6cba98
ariari01 <juh.shin@gmail.com>
2025-06-06 14:11:18 +0900

    myfeature2

--- src/test/java/ALUTest.java
+++ src/test/java/ALUTest.java
@@ -17,4 +17,18 @@ class ALUTest {
         assertEquals(30, ret.getResult());
         assertEquals(0, ret.getStatus());
     }
+
+    @Test
+    void enableSignal2() {
+        ALU alu = new ALU();
+        alu.setOperand1(10);
+        alu.setOperand2(-1);
+        alu.setOPCODE("ADD");
+
+        Result ret = new Result();
+        alu.enableSignal(ret);
+
+        assertEquals(65535, ret.getResult());
+        assertEquals(2, ret.getStatus());
+    }
 }
